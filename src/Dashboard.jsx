bd4c32d4f56579315f5cfa25289e3d029b6e18bb
Himanshu Yadav <yadavji3335@gmail.com>
2026-09-08 23:06:14 +0530

    Initial commit

---
+++ src/Dashboard.jsx
@@ -0,0 +1,34 @@
+import supabase from "./supabase-client.js";
+import { useEffect } from 'react';
+
+function Dashboard() {
+  useEffect(() => {
+    fetchMetrics();
+  }, []);
+  
+  async function fetchMetrics() {
+    // const { data, error } = 
+    const response = await supabase
+      .from('sales_deals')
+      .select(
+        `
+        name,
+        value
+        `,
+      )
+      .order('value', { ascending: false })
+      .limit(3)
+    console.log(response);
+  } 
+
+
+  return (
+    <div className="dashboard-wrapper">
+      <div className="chart-container">
+        <h2>Total Sales This Quarter ($)</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
